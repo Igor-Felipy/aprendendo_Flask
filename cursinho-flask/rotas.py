8e08c04d1bc281097cc31941ffa14de54cc61413
Igor-Felipy <igor.felipy123@gmail.com>
2020-06-26 19:54:11 -0400

    url dinamica

---
+++ cursinho-flask/rotas.py
@@ -0,0 +1,22 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "index"
+
+
+def teste():
+    return "<p>testando 1</p>"
+app.add_url_rule('/teste',"teste", teste)
+
+
+
+def teste2():
+    return "<h1>testando 2</h1>"
+app.add_url_rule('/teste2',"teste2", teste2)
+
+
+if __name__=="__main__":
+    app.run(debug=True, port="3000")
